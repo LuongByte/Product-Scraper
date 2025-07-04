8f8b6f778fb4a98a2c9efa67da08e855c993b4c8
Vinh Nguyen <luong_nguyen@sfu.ca>
2025-07-03 23:52:06 -0700

    Added sidebar for website filtering later and product selection

--- gui.py
+++ gui.py
@@ -15,13 +15,21 @@ class App:
         self.test_img = PhotoImage(file="test.png").subsample(4, 4)
 
         self.base.iconphoto(True, self.icon)
-        self.base.config(background='black')
+        self.sideicon = PhotoImage(file="drawing.png").subsample(10,10)
+        self.selected = "X"
 
-        self.label = Label(self.base, text='Deal Finder', font=('Arial', 40), relief='raised', bd='5', compound='top', image=self.test_img)
-        self.search_frame = Frame(self.base)
-        self.title_list = Listbox(self.base, font=('Arial', 15), background='white', height=15, width=100)
-        self.entry = Entry(self.search_frame, font=('Arial', 40))
-        self.button = Button(self.search_frame, text="Search", font=('Arial', 40), command=lambda: search(self.entry.get(), self.title_list))
+        style = tb.Style()
+        style.configure('Custom.TLabel', background="#303030", font=("Arial", 20))
+        style.configure('Custom.TCheckbutton', background="#303030", font=("Arial", 20))
+        
+        self.label = Label(base, text='Deal Finder', font=('Arial', 40), relief='raised', bd='5', compound='top', image=self.test_img)
+        self.search_frame = Frame(base)
+        self.sidebar = tb.Frame(base, style="Custom.TLabel", width=300)
+        self.bars = tb.Label(self.base, image=self.sideicon, cursor='hand2')
+        self.title_list = Listbox(base, font=('Arial', 15), background='white', height=15, width=75)
+        self.entry = Entry(self.search_frame, bg='white', fg='black', font=('Arial', 30))
+        self.button = Button(self.search_frame, text="Search", cursor='hand2', font=('Arial', 30), 
+                             command=lambda: search(self.entry.get(), self.title_list))
 
     
         def show_options(list, df):
@@ -51,7 +59,27 @@ class App:
             button2.after(100, lambda: button2.config(relief="raised"))
             button2.invoke()
 
+        def toggle_sidebar(event, sidebar):
+            if sidebar.winfo_manager() != 'place':
+                event.widget.configure(style="Custom.TLabel")
+                sidebar.place(x=0, y=0, relheight=1)
+            else:
+                event.widget.configure(style="")
+                sidebar.place_forget()
+
+        def on_click(event):
+            lists = event.widget
+            curr = lists.get(lists.curselection()) 
+            if(curr != self.selected):
+                self.selected = curr
+                print(self.selected)
+
         self.base.bind("<Return>", lambda event: on_enter(self.button))
+        self.title_list.bind("<<ListboxSelect>>", on_click)
+        self.bars.bind("<Button-1>", lambda event: toggle_sidebar(event, self.sidebar))
+
+        self.sidebar.pack_forget()
+        self.bars.place(x=10, y=10)
         self.label.pack()
         self.search_frame.pack()
         self.entry.pack(side=LEFT, padx=40)
